7a5325537dfb6da8df759cff6bf6d6ba7918c86c
Steven Harradine <stevenharradine@gmail.com>
2016-04-08 00:54:50 -0400

    write template

---
+++ templates/config.js
@@ -0,0 +1,4 @@
+module.exports.DATABASE_HOST          = {{ csp_database_host }}
+module.exports.DATABASE_USER          = {{ csp_database_user }}
+module.exports.DATABASE_PASSWORD      = {{ csp_database_password | mandatory }}
+module.exports.DATABASE_DATABASE_NAME = {{ csp_database_name }}
